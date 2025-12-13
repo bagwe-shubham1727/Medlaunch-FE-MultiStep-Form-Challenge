bbf4dceb40ea5e2efebcceda078824ec43184533
Shubham Bagwe <bagwe.sh@northeastern.edu>
2025-12-12 21:22:59 -0500

    added header component

---
+++ components/common/Header/Header.jsx
@@ -0,0 +1,17 @@
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.container}>
+        <h1 className={styles.title}>DNV Healthcare</h1>
+        <div className={styles.userInfo}>
+          <div className={styles.avatar}>KM</div>
+          <span className={styles.userName}>Katherine Martinez</span>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
